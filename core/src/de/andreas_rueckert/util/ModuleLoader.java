11e0bed55a92998132a27482c4e0e729547b90a7
daybyter <mail@andreas-rueckert.de>
2013-04-20 17:20:26 +0200

    Did some wotk on the ModuleLoader. Might better add a loop over the client constructors before creating a new client instance.

--- core/src/de/andreas_rueckert/util/ModuleLoader.java
+++ core/src/de/andreas_rueckert/util/ModuleLoader.java
@@ -25,11 +25,21 @@
 
 package de.andreas_rueckert;
 
+import de.andreas_rueckert.trade.site.TradeSite;
 import de.andreas_rueckert.util.LogUtils;
 import java.io.File;
 import java.io.FileFilter;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.net.URLDecoder;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.Map;
 
 
 /**
@@ -81,6 +91,12 @@ public class ModuleLoader {
 
 
     // Instance variables
+
+    /**
+     * A map of interfaces to trade sites. Since exchanges are often searched
+     * by their name, this is a hashmap< name, tradesite>.
+     */
+    private HashMap<String, TradeSite> _registeredTradeSites = new HashMap< String, TradeSite>();
 
 
     // Constructors
@@ -158,13 +174,119 @@ public class ModuleLoader {
 	// Maybe I should check, if this is actually a directory?
 	return extDir.listFiles( new JarFilenameFilter());
     }
+    
+    /**
+     * Get a registered trade site from it's name.
+     *
+     * @param siteName The name of the trading site.
+     *
+     * @return The trading site, or null, if no such site was registered.
+     */
+    public TradeSite getRegisteredTradeSite( String siteName) {
+        return _registeredTradeSites.get( siteName);
+    }
+
+    /**
+     * Get all the registered trade sites.
+     *
+     * @return The registered trade sites.
+     */
+    public Map<String,TradeSite> getRegisteredTradeSites() {
+        return _registeredTradeSites;
+    }
 
     /**
      * Load all the classes in a given jar file.
      * @see http://stackoverflow.com/questions/11016092/how-to-load-classes-at-runtime-from-a-folder-or-jar
      *
-     * @param jarFile The jar file with the classes.
+     * @param file The jar file with the classes.
      */
-    private void loadClassesFromJar( File jarFile) {
+    private void loadClassesFromJar( File file) {
+	
+	try {
+
+	    // Create a jar file from the given file.
+	    JarFile jarFile = new JarFile( file);
+	    
+	    // Create an URL array for the class loader.
+	    URL [] urls = { new URL("jar:file:" + file.getCanonicalPath() +"!/") };
+	    
+	    // Now we need a class loader.
+	    ClassLoader classLoader = URLClassLoader.newInstance( urls);
+
+	    // Loop over the entries of the jar file.
+	    for( Enumeration<JarEntry> enumeration = jarFile.entries(); enumeration.hasMoreElements(); ) {
+	    
+		// Get the next jar entry from the enumeration.
+		JarEntry jarEntry = enumeration.nextElement();
+		
+		if( !jarEntry.isDirectory() && jarEntry.getName().endsWith(".class")) {
+
+		    // -6 because of .class
+		    String className = jarEntry.getName().substring( 0, jarEntry.getName().length() - 6);
+		    
+		    // Convert path to package convention.
+		    className = className.replace( '/', '.');
+		    
+		    // Now load the class.
+		    Class loadedClass = classLoader.loadClass( className);
+		    
+		    // Now check, if this class is an exchange client implementation.
+		    if( loadedClass.isAnnotationPresent( TradeSite.class)) {
+			
+			// Create a client instance from the class.
+			// Since the client should have 0 arguments, this might work.
+			// But it might be better to loop over the available
+			// constructors and call the appropriate one?
+			TradeSite tradeSite = (TradeSite)( loadedClass.newInstance());
+
+			// And add it to the list of registered trade sites.
+			registerTradeSite( tradeSite);
+		    }
+		}
+	    }
+	} catch( IOException ioe) {
+
+	    // Log the raised error, while trying to access the file.
+	    LogUtils.getInstance().getLogger().error( "Cannot access jarfile in ModuleLoader: " + ioe);
+
+	} catch( ClassNotFoundException cnfe) {
+
+	    // Log, that the class loader cannot load a class.
+	    LogUtils.getInstance().getLogger().error( "ClassLoader in ModuleLoader cannot load a class: " + cnfe);
+
+	} catch( InstantiationException ie) {
+
+	    // The class loader cannot instantiate a loaded class.
+	    LogUtils.getInstance().getLogger().error( "ClassLoader in ModuleLoader cannot create exchange client implementation: " + ie);
+
+	} catch( IllegalAccessException iae) {
+
+	    // The class loader cannot a client implementation constructor (might be private?).
+	    LogUtils.getInstance().getLogger().error( "ClassLoader in ModuleLoader cannot access implementation constructor: " + iae);
+
+	}
+    }
+
+    /**
+     * Register a new trade site client.
+     *
+     * @param site The new trade site client to add to the list of sites.
+     */
+    private void registerTradeSite( TradeSite site) {
+
+        _registeredTradeSites.put( site.getName(), site);  // Add the trade site to the list of sites.
+        
+        // _chartProvider.registerTradeSite( site);  // Tell the chart provider about the new trade site.
+
+        // Keep the properties of the trade sites persistent.
+        // getAppProperties().registerPersistentPropertyObject( site);
+
+        /* if( ! _daemonMode) {
+            addTradeTablePanel( new TradeTable( site));  // Create UI elements for this trade site.
+	    } */
+
+	// Log the loading of the exchange client.
+        LogUtils.getInstance().getLogger().info( "Registered " + site.getName() + " interface");
     }
 }
