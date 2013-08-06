9e0501c0a9a1c70f8d7b6a28959097f577e65a35
Andreas Rueckert <mail@andreas-rueckert.de>
2013-08-06 16:26:33 +0200

    Started Bitcurex API implementation.

---
+++ modules/bitcurex/src/de/andreas_rueckert/trade/site/bitcurex/client/BitcurexClient.java
@@ -0,0 +1,347 @@
+/**
+ * Java implementation for cryptocoin trading.
+ *
+ * Copyright (c) 2013 the authors:
+ * 
+ * @author Andreas Rueckert <mail@andreas-rueckert.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining 
+ * a copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package de.andreas_rueckert.trade.site.bitcurex.client;
+
+import de.andreas_rueckert.NotYetImplementedException;
+import de.andreas_rueckert.trade.account.TradeSiteAccount;
+import de.andreas_rueckert.trade.CryptoCoinTrade;
+import de.andreas_rueckert.trade.Currency;
+import de.andreas_rueckert.trade.CurrencyImpl;
+import de.andreas_rueckert.trade.CurrencyNotSupportedException;
+import de.andreas_rueckert.trade.CurrencyPair;
+import de.andreas_rueckert.trade.CurrencyPairImpl;
+import de.andreas_rueckert.trade.Depth;
+import de.andreas_rueckert.trade.order.DepositOrder;
+import de.andreas_rueckert.trade.order.OrderStatus;
+import de.andreas_rueckert.trade.order.SiteOrder;
+import de.andreas_rueckert.trade.order.WithdrawOrder;
+import de.andreas_rueckert.trade.Price;
+import de.andreas_rueckert.trade.site.TradeSite;
+import de.andreas_rueckert.trade.site.TradeSiteImpl;
+import de.andreas_rueckert.trade.site.TradeSiteRequestType;
+import de.andreas_rueckert.trade.Ticker;
+import de.andreas_rueckert.trade.TradeDataNotAvailableException;
+import de.andreas_rueckert.util.HttpUtils;
+import java.util.ArrayList;
+import java.util.Collection;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONException;
+import net.sf.json.JSONObject;
+
+
+/**
+ * Main class for the bitcurex API.
+ *
+ * @see https://eur.bitcurex.com/reading-room/API
+ */
+public class BitcurexClient extends TradeSiteImpl implements TradeSite {
+
+    // Static variables
+
+    /**
+     * The domain of the service.
+     */
+    public static String DOMAIN = "eur.bitcurex.com";
+
+    /**
+     * Nonce for API request. Must be increased for each API call.
+     */
+    private static long _nonce;
+
+
+    // Instance variables
+
+    /**
+     * The API key.
+     */
+    private String _key = null;
+
+
+    // Constructors
+
+    /**
+     * Create a new connection to the Bitcurex website.
+     */
+    public BitcurexClient() {
+
+	super();  // Initialize the base class.
+
+	_name = "Bitcurex";
+	_url = "https://" + this.DOMAIN + "/";
+
+	// Define the supported currency pairs for this trading site.
+	_supportedCurrencyPairs = new CurrencyPair[1];
+	_supportedCurrencyPairs[0] = new CurrencyPairImpl( CurrencyImpl.BTC, CurrencyImpl.EUR);
+
+    }
+
+
+    // Methods
+
+    /**
+     * Execute an order on the trade site.
+     *
+     * @param order The order to execute.
+     *
+     * @return The new status of the order.
+     */
+    public OrderStatus executeOrder( SiteOrder order) {
+
+	throw new NotYetImplementedException( "Executing an order is not yet implemented for bitcurex");	
+    }
+
+    /**
+     * Get the current funds of the user.
+     *
+     * @return The accounts with the current balance as a collection of Account objects, or null if the request failed.
+     */
+    public Collection<TradeSiteAccount> getAccounts() {
+
+	throw new NotYetImplementedException( "Getting the accounts is not yet implemented for bitcurex");	
+    }
+
+    /**
+     * Get the market depth as a Depth object.
+     *
+     * @param currencyPair The queried currency pair.
+     *
+     * @throws TradeDataNotAvailableException if the depth is not available.
+     */
+    public Depth getDepth( CurrencyPair currencyPair) throws TradeDataNotAvailableException {
+
+	if( ! isSupportedCurrencyPair( currencyPair)) {
+	    throw new CurrencyNotSupportedException( "Currency pair: " + currencyPair.toString() + " is currently not supported on " + this._name);
+	}
+
+	String url = "https://" + this.DOMAIN + "/data/orderbook.json";
+
+	// Fetch the depth.
+	String requestResult = HttpUtils.httpGet( url);
+
+	if( requestResult != null) {  // Request sucessful?
+
+	    try {
+		// Convert the HTTP request return value to JSON to parse further.
+		return new BitcurexDepth( JSONObject.fromObject( requestResult), currencyPair, this);
+
+	    } catch( JSONException je) {
+		System.err.println( "Cannot parse " + this._name + " depth return: " + je.toString());
+
+		throw new TradeDataNotAvailableException( "cannot parse data from " + this._name);
+	    }
+	}
+	
+	throw new TradeDataNotAvailableException( this._name + " server did not respond to depth request");
+    }
+
+    /**
+     * Get the fee for an order in the resulting currency.
+     *
+     * @param order The order to use for the fee computation.
+     *
+     * @return The fee in the resulting currency (currency value for buy, payment currency value for sell).
+     */
+    public Price getFeeForOrder( SiteOrder order) {
+
+	if( order instanceof WithdrawOrder) {
+		    
+	    if( order.getCurrencyPair().getCurrency().equals( CurrencyImpl.BTC)) {
+
+		return new Price( "0.005", CurrencyImpl.BTC);  // Current Bitcurex withdraw fee...
+
+	    } else if( order.getCurrencyPair().getCurrency().equals( CurrencyImpl.EUR)) {
+
+		return new Price( "1.23", CurrencyImpl.EUR);  // Current Bitcurex withdraw fee..
+
+	    } else {
+
+		System.out.println( "Compute withdaw fees for currencies other than btc or eur");
+
+		throw new CurrencyNotSupportedException( "Cannot compute fee for this order: " + order.toString());
+	    }
+	} else if( order instanceof DepositOrder) {
+
+	    Currency depositedCurrency = ((DepositOrder)order).getCurrency();
+	    
+	    if( depositedCurrency.equals( CurrencyImpl.BTC)) {
+		
+		// BTC deposits are free as far as I know.
+		return new Price( "0.0", CurrencyImpl.BTC);
+	    
+	} else {
+
+		throw new NotYetImplementedException( "Deposit fees are not implemented for trade site " 
+						      + getName() 
+						      + " and currency " 
+						      + depositedCurrency.getName());
+	    }
+
+	} else {  // Bitcurex seems to have no trade fees?
+
+	    Currency tradedCurrency = order.getCurrencyPair().getCurrency();
+
+	    return new Price( "0.0", tradedCurrency);
+	}
+    }
+
+    /**
+     * Get the shortest allowed requet interval in microseconds.
+     *
+     * @return The shortest allowed request interval in microseconds.
+     */
+    public long getMinimumRequestInterval() {
+	return getUpdateInterval();
+    }
+
+    /**
+     * Get the open orders on this trade site.
+     *
+     * @return The open orders as a collection, or null if the request failed.
+     */
+    public Collection<SiteOrder> getOpenOrders() {
+
+	throw new NotYetImplementedException( "Getting the open orders is not yet implemented for bitcurex");	
+    }
+
+    /**
+     * Get the section name in the global property file.
+     *
+     * The name of the property section as a String.
+     */
+    public String getPropertySectionName() {
+	return "Bitcurex";
+    }
+
+    /**
+     * Get the current ticker from the bitcurex API.
+     *
+     * @param currencyPair The currency pair to query.
+     * @param paymentCurrency The currency for the payments.
+     *
+     * @return The current bitcurex ticker.
+     *
+     * @throws TradeDataNotAvailableException if the ticker is not available.
+     */
+    public Ticker getTicker( CurrencyPair currencyPair) throws TradeDataNotAvailableException {
+
+	if( ! isSupportedCurrencyPair( currencyPair)) {
+	    throw new CurrencyNotSupportedException( "Currency pair: " + currencyPair.toString() + " is currently not supported on Bitcurex");
+	}
+
+	String url = "https://" + this.DOMAIN + "/data/ticker.json";
+
+	String requestResult = HttpUtils.httpGet( url);
+
+	if( requestResult != null) {  // Request sucessful?
+
+	    try {
+
+		// Convert the HTTP request return value to JSON to parse further.
+		return new BitcurexTicker( JSONObject.fromObject( requestResult), currencyPair, this);
+		
+	    } catch( JSONException je) {
+		System.err.println( "Cannot parse ticker object: " + je.toString());
+	    }
+	}
+	
+	throw new TradeDataNotAvailableException( "The " + this._name + " ticker request failed");
+    }
+
+    /**
+     * Get a list of recent trades.
+     *
+     * @param since_micros The GMT-relative epoch in microseconds.
+     * @param currencyPair The currency pair to query.
+     *
+     * @return The trades as a list of Trade objects.
+     */
+    public CryptoCoinTrade [] getTrades( long since_micros, CurrencyPair currencyPair) throws TradeDataNotAvailableException {
+
+	if( ! isSupportedCurrencyPair( currencyPair)) {
+	    throw new CurrencyNotSupportedException( "Currency pair: " + currencyPair.toString() + " is currently not supported on Bitcurex");
+	}
+
+	String url = "https://" + this.DOMAIN + "/data/trades.json";
+
+	String requestResult = HttpUtils.httpGet( url);
+
+	if( requestResult != null) {  // If the HTTP request worked ok.
+
+	    ArrayList<CryptoCoinTrade> resultBuffer = new ArrayList<CryptoCoinTrade>();
+
+	    try {
+		// Convert the result to an JSON array.
+		JSONArray resultArray = JSONArray.fromObject( requestResult);
+		
+		// Iterate over the array and convert each trade from json to a Trade object.
+		for( int i = 0; i < resultArray.size(); i++) {
+
+		    JSONObject tradeObject = resultArray.getJSONObject(i);
+		    
+		    // Parse the json to create a new cryptocoin trade.
+		    CryptoCoinTrade newTrade = new BitcurexTradeImpl( tradeObject, this, currencyPair);
+		    
+		    // If the trade fits the given timespan, add it to the result buffer.
+		    if( newTrade.getTimestamp() > since_micros) {
+
+			resultBuffer.add( newTrade);  // Add the new Trade object to the list.
+		    }
+		}
+		
+		// Convert the list to an array and return it.
+		return resultBuffer.toArray( new CryptoCoinTrade[ resultBuffer.size()]); 
+
+	    } catch( JSONException je) {
+		System.err.println( "Cannot parse trade object: " + je.toString());
+	    }
+	}
+
+	return null;  // An error occurred.
+    }
+
+    /**
+     * Get the interval, in which the trade site updates it's depth, ticker etc. 
+     * in microseconds.
+     *
+     * @return The update interval in microseconds.
+     */
+    public long getUpdateInterval() {
+	return 30L * 1000000L;  // Bitcurex is quite low-volume, so this should be often enough.
+    }
+
+    /**
+     * Check, if some request type is allowed at the moment. Most
+     * trade site have limits on the number of request per time interval.
+     *
+     * @param requestType The type of request (trades, depth, ticker, order etc).
+     *
+     * @return true, if the given type of request is possible at the moment.
+     */
+    public boolean isRequestAllowed( TradeSiteRequestType requestType) {
+
+	return true;  // Just a dummy for now...
+    }
+}
---
+++ modules/bitcurex/src/de/andreas_rueckert/trade/site/bitcurex/client/BitcurexTicker.java
@@ -0,0 +1,103 @@
+/**
+ * Java implementation for cryptocoin trading.
+ *
+ * Copyright (c) 2013 the authors:
+ * 
+ * @author Andreas Rueckert <mail@andreas-rueckert.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining 
+ * a copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package de.andreas_rueckert.trade.site.bitcurex.client;
+
+import de.andreas_rueckert.trade.Amount;
+import de.andreas_rueckert.trade.CurrencyPair;
+import de.andreas_rueckert.trade.Price;
+import de.andreas_rueckert.trade.site.TradeSite;
+import de.andreas_rueckert.trade.TickerImpl;
+import net.sf.json.JSONException;
+import net.sf.json.JSONObject;
+
+
+/**
+ * This class implements a ticker object for the Bitcurex trading platform.
+ */
+public class BitcurexTicker extends TickerImpl {
+
+    // Static variables
+
+    /**
+     * The hash keys of the values in the array.
+     */
+    private static String [] HASHKEYS = { "high" ,"low", "avg", "vwap", "vol", "last", "buy", "sell", "time" };
+
+
+    // Instance variables
+
+    /**
+     * The traded volume.
+     */
+    private Amount _volume;
+
+
+    // Constructors
+
+    /**
+     * Create a new ticker object from the Bitcurex return value.
+     *
+     * @param jsonTicker A ticker state as a JSONObject.
+     * @param currencyPair The currency pair to query in this ticker object.
+     * @param tradeSite The trade site, this ticker belongs to.
+     */
+    public BitcurexTicker( JSONObject jsonTicker, CurrencyPair currencyPair, TradeSite tradeSite) throws JSONException {
+
+	super( currencyPair, tradeSite);  // Initialize the ticker functionality.
+
+	// Parse the ticker values in the json object and store the values in the hash map.
+	// The returned time is ignored for now, since we set the current time as the
+	// timestamp anyway...
+	for( String key : HASHKEYS) {
+
+	    if( key.equals( "vol")) {  // Store the volume as an amount, not as a price.
+
+		_volume = new Amount( jsonTicker.getString( key));  
+
+	    } else if( key.equals( "time")) {
+
+		// Ignore the time for now.
+
+	    } else {  // The other values should be prices.
+
+		_values.put( key, new Price( jsonTicker.getString( key)));
+	    }
+	}
+    }
+
+
+    // Methods
+
+    /**
+     * Get the volume field of this ticker.
+     *
+     * @return The volume field of this ticker as an Amount object.
+     */
+    public Amount getVolume() {
+
+	return _volume;
+    }
+}
